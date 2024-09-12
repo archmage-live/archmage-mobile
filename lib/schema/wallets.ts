431023e8d34daac8ba321b0e48a667db81f544fb
Deckard <103876178+deckard1982@users.noreply.github.com>
2024-09-12 09:19:28 +0800

    feat: add drizzle migration

---
+++ lib/schema/wallets.ts
@@ -0,0 +1,19 @@
+import { integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core'
+
+export const wallets = sqliteTable(
+  'wallets',
+  {
+    id: integer('id').primaryKey(),
+    sortId: integer('id').unique().notNull(),
+    name: text('name').unique().notNull(),
+  },
+  (wallets) => ({
+    nameIdx: uniqueIndex('nameIdx').on(wallets.name)
+  })
+)
+
+export type Wallet = typeof wallets.$inferSelect
+export type InsertWallet = typeof wallets.$inferInsert
+
+const a = {} as Wallet
+a.name
